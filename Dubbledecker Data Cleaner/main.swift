6a1095e1b453a1c4bb44aa2cc0b57840445251b3
Kish Patel <kish@kishpatel.com>
2019-05-27 23:27:27 +0100

    Initial Commit

---
+++ Dubbledecker Data Cleaner/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Dubbledecker Data Cleaner
+//
+//  Created by Kish Patel on 27/05/2019.
+//  Copyright © 2019 Kish Patel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
